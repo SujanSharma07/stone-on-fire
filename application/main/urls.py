21c041109b76900ea886f4b3acf13a23ca8291e0
SujanSharma07 <sujansharma202@gmail.com>
2024-07-12 15:10:47 +1000

    django to html

--- application/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import DashboardView, available_times, reservation_view
-
-urlpatterns = [
-    path("", DashboardView.as_view(), name="home"),
-    path("reservation/", reservation_view, name="reservation"),
-    path("reservations/available_times/", available_times, name="available_times"),
-]
